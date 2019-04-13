ce8fe5bb0486f646ab66f2bd8dc0d8f45eec19f2
Osman Yousufi <oyousufi@lcs.on.ca>
2019-04-13 10:13:26 -0400

    we got the keys working etc

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -14,6 +14,23 @@ public class Key extends Actor
      */
     public void act()
     {
+        
+        //animate the piano key being pressed 
+        if (Greenfoot.isKeyDown("g") )
+        {
+            //key is down
+            setImage("white-key-down.png");
+        }
+        else
+        {
+            //key is up
+            setImage("white-key.png")
+        }
+        
     }
+    
+    /**
+     * Play the note of this key.
+     */
 }
 
